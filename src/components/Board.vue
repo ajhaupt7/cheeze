c609558984784bf021e0d39c91d576685dcf46bd
Andrew Haupt <ajhaupt7@gmail.com>
2018-03-15 16:37:38 -0700

    Create board component

---
+++ src/components/Board.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="board">
+    <div
+      v-for = "(rank, i) in ranks"
+      :key  = "i"
+      class = "ranks"
+    >
+      <square
+        v-for  = "(file, j) in files"
+        :key   = "i + j"
+        :file  = "file"
+        :rank  = "rank"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import Square from './Square';
+
+export default {
+  name: 'board',
+  components: {
+    Square,
+  },
+  data() {
+    return {
+      files: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+      ranks: [8, 7, 6, 5, 4, 3, 2, 1],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.board {
+  width: 800px;
+  height: 800px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: nowrap;
+  align-items: stretch;
+}
+.ranks {
+  width: percentage(1/8);
+  height: 100%;
+}
+</style>
