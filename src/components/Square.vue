a912e25bf563c2e2641eea1c8ae6e5033401f0d0
Andrew Haupt <ajhaupt7@gmail.com>
2018-03-15 16:37:46 -0700

    Create square component

---
+++ src/components/Square.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="squareClass">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'square',
+  props: ['rank', 'file'],
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+    isDark() {
+      return (this.file.charCodeAt(0) + this.rank) % 2 === 0;
+    },
+    squareClass() {
+      return ['square', { dark: this.isDark }];
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.square {
+  background: blue;
+  width: 100%;
+  height: percentage(1/8);
+  &.dark {
+    background: red;
+  }
+}
+</style>
