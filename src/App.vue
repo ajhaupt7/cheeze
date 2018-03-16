21661f9c3c09fccb147133b88e90a7ac27248d02
Andrew Haupt <ajhaupt7@gmail.com>
2018-03-16 14:18:28 -0700

    Add sidebar to app layout + update layout styles
    Also remove board class of “moo” because the board is not a cow.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,38 @@
 <template>
   <div id="app">
-    <board class="moo" />
+    <div class="container">
+      <board />
+      <sidebar />
+    </div>
   </div>
 </template>
 
 <script>
 import Board from './components/Board';
+import Sidebar from './components/Sidebar';
 
 export default {
   name: 'App',
   components: {
     Board,
+    Sidebar,
   },
 };
 </script>
 
-<style>
+<style lang="scss">
+@import './assets/styles/app.scss';
 
+#app {
+  max-width: 1280px;
+  min-height: 100vh;
+  padding: $gutters;
+  margin: auto;
+}
+
+.container {
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+}
 </style>
