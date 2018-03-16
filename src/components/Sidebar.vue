75b380a59381c1cbc4589b45fe12375d525a8014
Andrew Haupt <ajhaupt7@gmail.com>
2018-03-16 14:17:27 -0700

    Add placeholder sidebar

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="sidebar">
+    <div class="sidebar-move-tracker">
+      <header>
+        Move Tracker
+      </header>
+      <article>
+
+      </article>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'sidebar',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/styles/variables.scss';
+
+.sidebar {
+  flex: 1;
+  display: inline-block;
+  margin-left: $gutters;
+  background: white;
+  border-radius: $corners;
+  box-shadow: $shadow;
+}
+
+header {
+  font-weight: $font-weight-thick;
+  font-size: $font-size-lg;
+  text-align: center;
+  background: $gray-dark;
+  padding: $gutters;
+  border-top-right-radius: $corners;
+  border-top-left-radius: $corners;
+  color: white;
+}
+
+article {
+  padding: $gutters;
+}
+</style>
