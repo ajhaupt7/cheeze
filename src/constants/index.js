8805d523f15bcc86e04b9a6a9efa9c11537dcc56
Andrew Haupt <ajhaupt7@gmail.com>
2018-03-16 20:14:55 -0700

    Add color constants

---
+++ src/constants/index.js
@@ -0,0 +1,57 @@
+export const COLORS = {
+  LIGHT: 'light',
+  DARK: 'dark',
+};
+
+export const INITIAL_COLORS = {
+  [COLORS.LIGHT]: '#efd9b7',
+  [COLORS.DARK]: '#b48866',
+};
+
+export const MATERIAL_COLOR_PALETTE = {
+  [COLORS.LIGHT]: [
+    INITIAL_COLORS[COLORS.LIGHT],
+    '#EF9A9A',
+    '#F48FB1',
+    '#CE93D8',
+    '#B39DDB',
+    '#9FA8DA',
+    '#90CAF9',
+    '#81D4FA',
+    '#80DEEA',
+    '#80CBC4',
+    '#A5D6A7',
+    '#C5E1A5',
+    '#E6EE9C',
+    '#FFF59D',
+    '#FFE082',
+    '#FFCC80',
+    '#FFAB91',
+    '#BCAAA4',
+    '#EEEEEE',
+    '#B0BEC5',
+  ],
+  [COLORS.DARK]: [
+    INITIAL_COLORS[COLORS.DARK],
+    '#D32F2F',
+    '#C2185B',
+    '#7B1FA2',
+    '#512DA8',
+    '#303F9F',
+    '#1976D2',
+    '#0288D1',
+    '#0097A7',
+    '#00796B',
+    '#388E3C',
+    '#689F38',
+    '#AFB42B',
+    '#FBC02D',
+    '#FFA000',
+    '#F57C00',
+    '#E64A19',
+    '#5D4037',
+    '#616161',
+    '#455A64',
+  ],
+};
+
