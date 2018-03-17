199e9863ac9f3e7ba395e3e86916932a8ca4eb83
Andrew Haupt <ajhaupt7@gmail.com>
2018-03-17 09:45:43 -0700

    Add support for updating board colors and resetting moves

--- src/store/actions.js
+++ src/store/actions.js
@@ -12,3 +12,30 @@ export const registerMove = ({ commit, state }, square) => {
     data: [...state.moves, square],
   });
 };
+
+/**
+ *
+ * @param context
+ * @param {Object} data - key should be the color
+ *   type (i.e. COLORS.LIGHT or COLORS.DARK) and value
+ *   should be the color string.
+ */
+export const updateColor = ({ commit, state }, data) => {
+  commit('setData', {
+    type: 'colors',
+    data: { ...state.colors, ...data },
+  });
+};
+
+/**
+ * Clears the array keeping track of moves.
+ *
+ * @param context
+ */
+export const resetMoves = ({ commit }) => {
+  commit('setData', {
+    type: 'moves',
+    data: [],
+  });
+};
+
